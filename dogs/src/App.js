90a0b2e4b9408295eddff9eac3b09d6e65c2286a
MateusFuzer <mateus_fuzer@outlook.com>
2021-10-25 11:34:00 -0300

    criado header

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,8 +1,22 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './app.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
 function App() {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
